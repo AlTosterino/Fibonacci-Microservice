4a3a415859823af4f43364f5e1d25af8feb091dc
AlTosterino <altosterino@gmail.com>
2020-05-06 11:57:13 +0200

    Add sender to use  case

--- src/fib_microservice/application/use_cases/generator.py
+++ src/fib_microservice/application/use_cases/generator.py
@@ -5,16 +5,22 @@ LOOP = asyncio.get_event_loop()
 
 log = logging.getLogger(__name__)
 
+from fib_microservice.infrastructure.message_broker.sender import Sender
 
-async def main_loop(generator: "FibonacciGenerator") -> None:
+
+async def main_loop(sender: "Sender", generator: "FibonacciGenerator") -> None:
     async for num in generator:
         log.info("Generated: %s", num)
+        log.info("Sending...")
+        sender.send_message(num)
+        log.info("Sent")
 
 
 def main(settings: "GeneratorSettings") -> None:
     generator = settings.generator
+    sender = settings.sender
     try:
-        LOOP.run_until_complete(main_loop(generator))
+        LOOP.run_until_complete(main_loop(sender, generator))
     except KeyboardInterrupt:
         log.info("Got EXIT signal")
     finally:
