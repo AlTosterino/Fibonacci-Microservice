25d5f4fb601fae8b50349384cc8d9ca47098fa78
Daniel Różycki <daniel.rozycki@stxnext.pl>
2020-05-06 09:16:04 +0200

    Add entrypoint

--- src/fib_microservice/run_app.py
+++ src/fib_microservice/run_app.py
@@ -4,37 +4,13 @@ import logging
 
 from fib_microservice import __version__
 from fib_microservice.shared.settings import Settings
+from fib_microservice.application.use_cases import generator
 
 __author__ = "AlTosterino"
 __copyright__ = "AlTosterino"
 __license__ = "mit"
 
 log = logging.getLogger(__name__)
-
-
-def fib(n):
-    """Fibonacci example function
-
-    Args:
-      n (int): integer
-
-    Returns:
-      int: n-th Fibonacci number
-    """
-    assert n > 0
-    a, b = 1, 1
-    for i in range(n - 1):
-        a, b = b, a + b
-    return a
-
-
-def main(settings: Settings) -> None:
-    """Main entry point allowing external calls
-
-    Args:
-        settings ([Settings]): Settings object
-    """
-    log.debug("Launched")
 
 
 def parse_args():
@@ -46,28 +22,49 @@ def parse_args():
     Returns:
       :obj:`argparse.Namespace`: command line parameters namespace
     """
-    parser = argparse.ArgumentParser(description="Just a Fibonacci demonstration")
+    parser = argparse.ArgumentParser(description="Fibonacci microservice")
     parser.add_argument(
         "--version",
         action="version",
         version="fib_microservice {ver}".format(ver=__version__),
     )
-    parser.add_argument("--debug", help="Debug file", type=str, metavar="Path")
-
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("-g", "--generator", nargs="?", const=3, type=int)
-    group.add_argument("-c", "--consumer", action="store_true")
-    group.add_argument("-a", "--api", action="store_true")
+    parser.add_argument(
+        "-c",
+        "--config",
+        help="YAML config file",
+        type=str,
+        metavar="Path",
+        required=True,
+    )
     return parser.parse_args()
 
 
-def run():
-    """Entry point for console_scripts"""
-    args = parse_args()
-    settings = Settings.from_args()
+def configure_settings(args: argparse.Namespace) -> Settings:
+    settings = Settings.from_yaml(args)
     settings.setup_log()
-    main(settings)
+    return settings
 
 
-if __name__ == "__main__":
-    run()
+def run_generator() -> None:
+    """Entry point for generator"""
+    args = parse_args()
+    settings = configure_settings(args)
+    log.info("Launching generator")
+    try:
+        generator.main(settings.generator)
+    except Exception as e:
+        log.exception(e)
+
+
+def run_consumer() -> None:
+    """Entry point for consumer"""
+    args = parse_args()
+    settings = configure_settings(args)
+    log.info("Launching consumer")
+
+
+def run_api() -> None:
+    """Entry point for api"""
+    args = parse_args()
+    settings = configure_settings(args)
+    log.info("Launching api")
