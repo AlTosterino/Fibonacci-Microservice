6c225bb34fd5e43507b37d009786ac2c62a8540a
AlTosterino <altosterino@gmail.com>
2020-05-10 09:43:14 +0200

    Add sending

--- src/fib_microservice/application/use_cases/generator.py
+++ src/fib_microservice/application/use_cases/generator.py
@@ -5,14 +5,14 @@ LOOP = asyncio.get_event_loop()
 
 log = logging.getLogger(__name__)
 
-from fib_microservice.infrastructure.message_broker.sender import Sender
 
-
-async def main_loop(sender: "Sender", generator: "FibonacciGenerator") -> None:
+async def main_loop(
+    sender: "SenderRepository", generator: "FibonacciGenerator"
+) -> None:
     async for num in generator:
         log.info("Generated: %s", num)
         log.info("Sending...")
-        sender.send_message(num)
+        await sender.send_message(num)
         log.info("Sent")
 
 
